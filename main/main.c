9668c9f3d9126bd8f5b839976624d71ed3d47162
雨霖铃 <rsgltzyd@hotmail.com>
2026-01-11 23:19:38 +0800

    Refactor code structure for improved readability and maintainability

---
+++ main/main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <inttypes.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "driver/gpio.h"
+
+static QueueHandle_t gpio_evt_queue = NULL;
+
+static void IRAM_ATTR gpio_isr_handler(void* arg)
+{
+    uint32_t gpio_num = (uint32_t) arg;
+    xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
+}
+
+static void gpio_task_example(void* arg)
+{
+    uint32_t io_num;
+    for(;;) {
+        if(xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY)) {
+            printf("GPIO[%"PRIu32"] intr, val: %d\n", io_num, gpio_get_level(io_num));
+        }
+    }
+}
+
+void app_main(void)
+{
+    gpio_config_t io_conf = {
+        .intr_type = GPIO_INTR_NEGEDGE, //falling edge interrupt
+        .mode = GPIO_MODE_INPUT, //set as input mode
+        .pin_bit_mask = 1<<GPIO_NUM_0, //bit mask of the pins GPIO0
+        .pull_down_en = 0, //disable pull-down mode
+        .pull_up_en = 1 //enable pull-up mode
+    };
+    //configure GPIO with the given settings
+    gpio_config(&io_conf);
+
+    //create a queue to handle gpio event from isr
+    gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
+    //start gpio task
+    xTaskCreate(gpio_task_example, "gpio_task_example", 2048, NULL, 10, NULL);
+
+    //install gpio isr service
+    gpio_install_isr_service(0);
+    //hook isr handler for specific gpio pin
+    gpio_isr_handler_add(GPIO_NUM_0, gpio_isr_handler, (void*) GPIO_NUM_0);
+}
