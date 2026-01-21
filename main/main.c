a1751ca10d4bc8b37c7e33c58ec7da57f72d0b6a
雨霖铃 <rsgltzyd@hotmail.com>
2026-01-21 21:00:43 +0800

    Refactors GPIO setup and improves code clarity
    Updates GPIO configuration to use clearer variable names and adds explanatory comments to improve maintainability. Removes unused header includes.
    
    Enhances code readability for future development and debugging.

--- main/main.c
+++ main/main.c
@@ -1,49 +1,48 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 #include <inttypes.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/queue.h"
 #include "driver/gpio.h"
 
-static QueueHandle_t gpio_evt_queue = NULL;
+static QueueHandle_t gpio_evt_queue = NULL;  // 定义队列句柄
 
-static void IRAM_ATTR gpio_isr_handler(void* arg)
+// GPIO中断服务函数
+static void IRAM_ATTR gpio_isr_handler(void* arg) 
 {
-    uint32_t gpio_num = (uint32_t) arg;
-    xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
+    uint32_t gpio_num = (uint32_t) arg;  // 获取入口参数
+    xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL); // 把入口参数值发送到队列
 }
 
+// GPIO任务函数
 static void gpio_task_example(void* arg)
 {
-    uint32_t io_num;
+    uint32_t io_num; // 定义变量 表示哪个GPIO
     for(;;) {
-        if(xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY)) {
-            printf("GPIO[%"PRIu32"] intr, val: %d\n", io_num, gpio_get_level(io_num));
+        if(xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY)) {  // 死等队列消息
+            printf("GPIO[%"PRIu32"] intr, val: %d\n", io_num, gpio_get_level(io_num)); // 打印相关内容
         }
     }
 }
 
 void app_main(void)
 {
-    gpio_config_t io_conf = {
-        .intr_type = GPIO_INTR_NEGEDGE, //falling edge interrupt
-        .mode = GPIO_MODE_INPUT, //set as input mode
-        .pin_bit_mask = 1<<GPIO_NUM_0, //bit mask of the pins GPIO0
-        .pull_down_en = 0, //disable pull-down mode
-        .pull_up_en = 1 //enable pull-up mode
+    gpio_config_t io0_conf = {
+        .intr_type = GPIO_INTR_NEGEDGE, // 下降沿中断
+        .mode = GPIO_MODE_INPUT, // 输入模式
+        .pin_bit_mask = 1<<GPIO_NUM_0, // 选择GPIO0
+        .pull_down_en = 0, // 禁能内部下拉
+        .pull_up_en = 1 // 使能内部上拉
     };
-    //configure GPIO with the given settings
-    gpio_config(&io_conf);
+    // 根据上面的配置 设置GPIO
+    gpio_config(&io0_conf);
 
-    //create a queue to handle gpio event from isr
+    // 创建一个队列初始GPIO事件
     gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
-    //start gpio task
+    // 开启GPIO任务
     xTaskCreate(gpio_task_example, "gpio_task_example", 2048, NULL, 10, NULL);
-
-    //install gpio isr service
+    // 创建GPIO中断服务
     gpio_install_isr_service(0);
-    //hook isr handler for specific gpio pin
+    // 给GPIO0添加中断处理
     gpio_isr_handler_add(GPIO_NUM_0, gpio_isr_handler, (void*) GPIO_NUM_0);
 }
